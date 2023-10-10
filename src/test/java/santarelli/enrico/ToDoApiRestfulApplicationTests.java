cb2f7c8dc98a219b4696fa67737d13a2f6bd3969
EnricoSantarelli <enricosantarelli@hotmail.com>
2023-10-10 17:52:36 -0300

    Initial commit

---
+++ src/test/java/santarelli/enrico/ToDoApiRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package santarelli.enrico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApiRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
